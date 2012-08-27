e4a535a49aa01e9246569b805b21d109c66858f5
Kim Min Soo <ms.kim8717@gmail.com>
2012-08-27 09:12:07 +0900

    modified test_depthInfo.cpp

--- src/test_depthInfo.cpp
+++ src/test_depthInfo.cpp
@@ -13,6 +13,7 @@ class DepthInfo
   ros::NodeHandle nh_;
 
   image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
   image_transport::Subscriber depth_sub_;
   image_transport::Publisher depth_pub_;
 
@@ -26,18 +27,15 @@ public:
   {
     depth_pub_ = it_.advertise("out", 1);
     depth_sub_ = it_.subscribe("camera/depth/image", 1, &DepthInfo::depthInfoCb, this);
+    image_sub_ = it_.subscribe("camera/rgb/image_color", 1, &DepthInfo::imageCb, this);
   }
 
-
-  void depthInfoCb(const sensor_msgs::ImageConstPtr& msg)
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     cv_bridge::CvImagePtr cv_ptr_rgb;
-    cv_bridge::CvImagePtr cv_ptr_depth;
-
     try
     {
       cv_ptr_rgb = cv_bridge::toCvCopy(msg, enc::BGR8);
-      cv_ptr_depth = cv_bridge::toCvCopy(msg, enc::TYPE_32FC1);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -46,11 +44,26 @@ public:
     }
 
     rgb_frame = cv_ptr_rgb->image;
-    depth_frame = cv_ptr_depth->image;
-
     cv::imshow("rgb_image", rgb_frame);
-    cv::imshow("depth_image", depth_frame);
     cv::waitKey(3);
+  }
+
+  void depthInfoCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr_depth;
+
+    try
+    {
+      cv_ptr_depth = cv_bridge::toCvCopy(msg, enc::TYPE_32FC1);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    depth_frame = cv_ptr_depth->image;
+    cv::imshow("depth_image", depth_frame);
 
     depth_pub_.publish(cv_ptr_depth->toImageMsg());
 
